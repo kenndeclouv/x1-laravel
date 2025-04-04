dd8292a6bf526c8949096008a7fcd091d9d9787f
kenndeclouv <zuuliziluuz@gmail.com>
2025-04-04 07:02:18 +0700

    feattt

--- resources/views/components/partials/seo.blade.php
+++ resources/views/components/partials/seo.blade.php
@@ -1,16 +1,16 @@
 <meta name="description"
-    content="Join the X1 Community to connect with like-minded individuals, share insights, and stay updated on the latest news and events.">
+    content="Experience the best Minecraft adventure with us! Join the X1 Community to connect with like-minded individuals, share insights, and stay up-to-date with the latest news and events.">
 <meta name="author" content="kenndeclouv">
-<meta name="keywords" content="X1, community, gaming, discord, minecraft">
+<meta name="keywords" content="X1, community, gaming, discord, minecraft, server, adventure, survival, creative, roleplay, PvP, PvE, multiplayer, online, game, players, join, play, fun, social, chat, forums, events, news, updates, mods, plugins, custom, maps, resourcepacks, skins, texturepacks, plugins, commands, tutorials, guides, wiki, documentation, support, help, faq, terms, conditions, privacy, policy">
 
-<meta property="og:title" content="X1 Community - Your Hub for All Things">
+<meta property="og:title" content="X1 Community - Minecraft Server">
 <meta property="og:description"
-    content="Join the X1 Community to connect with like-minded individuals, share insights, and stay updated on the latest news and events.">
-<meta property="og:image" content="URL_to_image.jpg">
+    content="Experience the best Minecraft adventure with us! Join the X1 Community to connect with like-minded individuals, share insights, and stay up-to-date with the latest news and events.">
+<meta property="og:image" content="{{ asset('og_image.png') }}">
 <meta property="og:url" content="{{ request()->url() }}">
 
 <meta name="twitter:card" content="summary_large_image">
-<meta name="twitter:title" content="X1 Community - Your Hub for All Things X1">
+<meta name="twitter:title" content="X1 Community - Minecraft Server">
 <meta name="twitter:description"
-    content="Join the X1 Community to connect with like-minded individuals, share insights, and stay updated on the latest news and events.">
-<meta name="twitter:image" content="URL_to_image.jpg">
+    content="Experience the best Minecraft adventure with us! Join the X1 Community to connect with like-minded individuals, share insights, and stay up-to-date with the latest news and events.">
+<meta name="twitter:image" content="{{ asset('og_image.png') }}">
